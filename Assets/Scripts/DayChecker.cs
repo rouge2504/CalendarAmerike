3fbce7590e1d63cc6209eca20549641cc7dda527
rouge2504 <rouge2504@gmail.com>
2021-01-24 16:42:29 -0600

    Set Main

---
+++ Assets/Scripts/DayChecker.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DayChecker : MonoBehaviour
+{
+
+    public ScrollRect scrollRect;
+    public RectTransform contentPanel;
+    public GameObject hourToClone;
+
+    private List<Hour> hoursList;
+
+    public Color stayHourColor;
+
+    public float repeatTimeOnMinutesToUpdate;
+
+    //public RectTransform target;
+
+
+    void Start()
+    {
+        SetHours();
+        InvokeRepeating("UpdateList", 0, repeatTimeOnMinutesToUpdate * 60);
+        //SnapTo(target);
+    }
+
+    private void SetHours()
+    {
+        var hours = Enumerable.Range(00, 24).Select(i => (DateTime.MinValue.AddHours(i)).ToString("hh.mm tt")).ToArray();
+
+        hoursList = new List<Hour>();
+
+        for (int i = 0; i < hours.Count(); i++)
+        {
+            GameObject clone = Instantiate(hourToClone, contentPanel.GetComponent<Transform>());
+            clone.GetComponent<Hour>().SetContent(hours[i], "", hours[i]);
+            hoursList.Add(clone.GetComponent<Hour>());
+        }
+
+
+
+
+    }
+
+    private void UpdateList()
+    {
+        DateTime date = DateTime.Now;
+
+        for (int i = 0; i < hoursList.Count(); i++)
+        {
+
+            string validateMeridian = date.ToString("tt");
+            string validateHour = date.ToString("%h");
+            if (hoursList[i].nameHour.Contains(validateMeridian) && hoursList[i].nameHour.Contains(validateHour))
+            {
+                SnapTo(hoursList[i].GetComponent<RectTransform>());
+                SetColor(hoursList[i].GetComponent<Image>());
+            }
+        }
+
+    }
+
+    public void SetColor(Image target)
+    {
+        target.color = stayHourColor;
+    }
+
+    public void SnapTo(RectTransform target)
+    {
+        Canvas.ForceUpdateCanvases();
+
+        contentPanel.anchoredPosition =
+            (Vector2)scrollRect.transform.InverseTransformPoint(contentPanel.position)
+            - (Vector2)scrollRect.transform.InverseTransformPoint(target.position);
+    }
+}
---
+++ Assets/Scripts/Hour.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Hour : MonoBehaviour
+{
+    public Text dayNumber;
+    public Text contentText;
+    [HideInInspector] public string nameHour;
+
+    public void SetContent(string dayNumber, string contentText, string  nameHour)
+    {
+        this.dayNumber.text = dayNumber;
+        this.contentText.text = contentText;
+        this.nameHour = nameHour;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
